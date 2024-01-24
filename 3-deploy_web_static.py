8ae7cb5b7a8afa3268a3ecc19f8048bfb6679dc9
Epaphradito Lugayavu <paphra.me@gmail.com>
2024-01-24 14:53:11 +0300

    Changes

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import local, env, put, run, sudo
 from datetime import datetime
 import os
 
-# env.hosts = ['100.25.119.244', '54.89.107.222']
+env.hosts = ['100.25.119.244', '54.89.107.222']
 
 
 def do_pack():
@@ -15,16 +15,21 @@ def do_pack():
     Returns:
         None: if there is a problem, otherwise, the location of the archive
     """
-    try:
-        local("mkdir -p versions")
+    if not hasattr(env, 'packed_archive'):
+        try:
+            local("mkdir -p versions")
 
-        time_format = "%Y%m%d%H%M%S"
-        archive_name = "web_static_{}.tgz".format(
-                datetime.utcnow().strftime(time_format))
-        local("tar -cvzf versions/{} web_static".format(archive_name))
-        return os.path.join("versions", archive_name)
-    except Exception as e:
-        return None
+            time_format = "%Y%m%d%H%M%S"
+            archive_name = "web_static_{}.tgz".format(
+                    datetime.utcnow().strftime(time_format))
+            local("tar -cvzf versions/{} web_static".format(archive_name))
+            archive_path = os.path.join("versions", archive_name)
+            env.packed_archive = archive_path
+            return archive_path
+        except Exception as e:
+            return None
+    else:
+        return env.packed_archive
 
 
 def do_deploy(archive_path):
@@ -49,8 +54,7 @@ def do_deploy(archive_path):
             archive_filename + ".tgz", archive_filename))
         run("rm /tmp/{}".format(archive_filename + ".tgz"))
         run("mv /data/web_static/releases/{}/web_static/* \
-                /data/web_static/releases/{}/".format(
-                    archive_filename, archive_filename))
+/data/web_static/releases/{}/".format(archive_filename, archive_filename))
         run("rm -rf /data/web_static/releases/{}/web_static".format(
             archive_filename))
         run("rm -rf /data/web_static/current")
@@ -72,10 +76,4 @@ def deploy():
     if path is None:
         return False
 
-    env.host_string = '100.25.119.244'
-    if not do_deploy(path):
-        return False
-    env.host_string = '54.89.107.222'
-    if not do_deploy(path):
-        return False
-    return True
+    return do_deploy(path)
