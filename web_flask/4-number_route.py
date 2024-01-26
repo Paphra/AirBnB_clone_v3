b1a83212ed3f707eae633b56ff4861d98576103a
Epaphradito Lugayavu <paphra.me@gmail.com>
2024-01-26 23:39:31 +0300

    Is a number check

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""number_route module
+Starts a Flask web application
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """hello_hbnb function
+    Defines what is return by / route
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """hbnb function
+    Defines what is returned by /hbnb route
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """c_route function
+    Defines the return value of /c/<text> route
+    """
+    processed = text.replace('_', ' ')
+    return 'C {}'.format(processed)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text="is cool"):
+    """python_route function
+    Defines the return value of /python/<text> route
+    """
+    processed = text.replace('_', ' ')
+    return 'Python {}'.format(processed)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    """number_route function
+    Defines the route of /number/<n>
+    Works well if n is a number
+    """
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
